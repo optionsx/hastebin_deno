aab5cd795cf8ee0002b9b0e33e50a40a38d70f36
optionsx <fake@protonmail.com>
2023-03-02 05:14:43 +0300

    _

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import axios from "https://deno.land/x/axiod@0.26.2/mod.ts";
+import axios from "https://deno.land/x/axiod/mod.ts";
 import { hasteBin } from "./core/hastebin.ts";
 
 export { axios };
